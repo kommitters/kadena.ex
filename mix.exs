94efc3fac9e6bacedb74cdf3496bdeeb64e02372
Santiago Botero <98826652+santiagoboterokommit@users.noreply.github.com>
2022-09-08 15:30:19 -0500

    Setup CI/CD with Github actions (#7)
    * Add CI/CD with dialyzer, credo and test
    
    * Remove docs files

--- mix.exs
+++ mix.exs
@@ -1,13 +1,24 @@
 defmodule Kadena.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @github_url "https://github.com/kommitters/kadena"
+
   def project do
     [
       app: :kadena,
-      version: "0.1.0",
-      elixir: "~> 1.12",
+      version: @version,
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "Kadena",
+      description: description(),
+      source_url: @github_url,
+      package: package(),
+      dialyzer: [
+        plt_add_apps: [:kadena, :ex_unit],
+        plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
+      ]
     ]
   end
 
@@ -21,8 +32,28 @@ defmodule Kadena.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.24", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description do
+    """
+    Elixir library to interact with the Kadena blockchain.
+    """
+  end
+
+  defp package do
+    [
+      description: description(),
+      files: ["lib", "config", "mix.exs", "README*", "LICENSE"],
+      licenses: ["MIT"],
+      links: %{
+        "Changelog" => "#{@github_url}/blob/master/CHANGELOG.md",
+        "GitHub" => @github_url,
+        "Sponsor" => "https://github.com/sponsors/kommitters"
+      }
     ]
   end
 end
