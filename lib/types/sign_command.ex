6ad2a8a4ed34175ef2d85581c25a08bdc0cb4f82
Santiago Botero <santiago.botero@kommit.co>
2022-09-21 10:04:47 -0500

    Improve sign_command to receive directly hash, sig and pub_key
    Co-authored-by: Cristhian Rodriguez <50219561+CristhianRodriguezMolina@users.noreply.github.com>

--- lib/types/sign_command.ex
+++ lib/types/sign_command.ex
@@ -3,7 +3,12 @@ defmodule Kadena.Types.SignCommand do
   `SignCommand` struct definition.
   """
 
-  alias Kadena.Types.SignatureWithHash
+  alias Kadena.Types.{
+    Signature,
+    SignatureWithHash,
+    SignedSignatureWithHash,
+    UnsignedSignatureWithHash
+  }
 
   @behaviour Kadena.Types.Spec
 
@@ -15,5 +20,25 @@ defmodule Kadena.Types.SignCommand do
 
   @impl true
   def new(%SignatureWithHash{} = signature), do: %__MODULE__{command: signature}
-  def new(_sig), do: {:error, :invalid_sign_command}
+
+  def new(args) when is_list(args) do
+    hash = Keyword.get(args, :hash, nil)
+    sig = Keyword.get(args, :sig, nil)
+    pub_key = Keyword.get(args, :pub_key, nil)
+
+    case validate(hash, sig, pub_key) do
+      {:error, _error} -> {:error, :invalid_sign_command}
+      signature -> %__MODULE__{command: SignatureWithHash.new(signature)}
+    end
+  end
+
+  def new(_args), do: {:error, :invalid_sign_command}
+
+  @spec validate(hash :: String.t(), sig :: Signature.t(), pub_key :: String.t()) ::
+          SignedSignatureWithHash.t() | UnsignedSignatureWithHash.t() | {:error, atom()}
+  defp validate(hash, sig, nil),
+    do: UnsignedSignatureWithHash.new(hash: hash, sig: sig, pub_key: nil)
+
+  defp validate(hash, sig, pub_key),
+    do: SignedSignatureWithHash.new(hash: hash, sig: sig, pub_key: pub_key)
 end
--- test/types/sign_command_test.exs
+++ test/types/sign_command_test.exs
@@ -5,23 +5,58 @@ defmodule Kadena.Types.SignCommandTest do
 
   use ExUnit.Case
 
-  alias Kadena.Types.{Signature, SignatureWithHash, SignCommand, SignedSignatureWithHash}
+  alias Kadena.Types.{
+    Signature,
+    SignatureWithHash,
+    SignCommand,
+    SignedSignatureWithHash,
+    UnsignedSignatureWithHash
+  }
 
   describe "new/1" do
-    test "with a valid SignatureWithHash" do
-      signature =
-        SignedSignatureWithHash.new(
-          hash: "JHgnKe5Wd4hNIb7a6bIhm4ifxsYFzVGtAMyi_TEO-oM",
-          sig:
-            Signature.new(
-              "13a8c30a12077831a4e458f653850bcee75aec442075d24bfb6d5c54c0e5bd59deaa2b2301a99f26d15ec32ad3a581352430f163cf9401d07ce132f7b38df00e"
-            ),
-          pub_key: "ba54b224d1924dd98403f5c751abdd10de6cd81b0121800bf7bdbdcfaec7388d"
-        )
+    setup do
+      %{
+        hash: "JHgnKe5Wd4hNIb7a6bIhm4ifxsYFzVGtAMyi_TEO-oM",
+        sig:
+          Signature.new(
+            "13a8c30a12077831a4e458f653850bcee75aec442075d24bfb6d5c54c0e5bd59deaa2b2301a99f26d15ec32ad3a581352430f163cf9401d07ce132f7b38df00e"
+          ),
+        pub_key: "ba54b224d1924dd98403f5c751abdd10de6cd81b0121800bf7bdbdcfaec7388d"
+      }
+    end
+
+    test "with a valid SignatureWithHash", %{hash: hash, sig: sig, pub_key: pub_key} do
+      signature = SignedSignatureWithHash.new(hash: hash, sig: sig, pub_key: pub_key)
 
       signature_with_hash = SignatureWithHash.new(signature)
 
       %SignCommand{command: ^signature_with_hash} = SignCommand.new(signature_with_hash)
+    end
+
+    test "with a valid UnsignatureWithHash", %{hash: hash, sig: sig} do
+      signature = UnsignedSignatureWithHash.new(hash: hash, sig: sig)
+
+      signature_with_hash = SignatureWithHash.new(signature)
+
+      %SignCommand{command: ^signature_with_hash} = SignCommand.new(signature_with_hash)
+    end
+
+    test "with the auto-check for signed signature", %{hash: hash, sig: sig, pub_key: pub_key} do
+      %SignCommand{command: %SignatureWithHash{signature: %SignedSignatureWithHash{}}} =
+        SignCommand.new(hash: hash, sig: sig, pub_key: pub_key)
+    end
+
+    test "with the auto-check for unsigned signature", %{hash: hash, sig: sig} do
+      %SignCommand{command: %SignatureWithHash{signature: %UnsignedSignatureWithHash{}}} =
+        SignCommand.new(hash: hash, sig: sig)
+    end
+
+    test "with an incomplete list", %{hash: hash, pub_key: pub_key} do
+      {:error, :invalid_sign_command} = SignCommand.new(hash: hash, pub_key: pub_key)
+    end
+
+    test "with an invalid hash in list", %{sig: sig, pub_key: pub_key} do
+      {:error, :invalid_sign_command} = SignCommand.new(hash: 123, sig: sig, pub_key: pub_key)
     end
 
     test "with a string value" do
