7622c5dd49b9d15bc69709bdbbfbeb5401501fe8
Santiago Botero <98826652+santiagoboterokommit@users.noreply.github.com>
2022-10-04 00:32:35 -0500

    Add PactExec struct definition

--- lib/types/continuation.ex
+++ lib/types/continuation.ex
@@ -7,13 +7,13 @@ defmodule Kadena.Types.Continuation do
 
   @behaviour Kadena.Types.Spec
 
-  @type def :: String.t()
+  @type continuation_def :: String.t()
   @type args :: PactValue.t()
-  @type value :: def() | args()
+  @type value :: continuation_def() | args()
   @type validation :: {:ok, value()} | {:error, Keyword.t()}
 
   @type t :: %__MODULE__{
-          def: def(),
+          def: continuation_def(),
           args: args()
         }
 
@@ -21,18 +21,18 @@ defmodule Kadena.Types.Continuation do
 
   @impl true
   def new(args) do
-    def = Keyword.get(args, :def)
+    continuation_def = Keyword.get(args, :def)
     args = Keyword.get(args, :args)
 
-    with {:ok, def} <- validate_def(def),
+    with {:ok, continuation_def} <- validate_def(continuation_def),
          {:ok, args} <- validate_args(args) do
-      %__MODULE__{def: def, args: args}
+      %__MODULE__{def: continuation_def, args: args}
     end
   end
 
-  @spec validate_def(def :: def()) :: validation()
-  def validate_def(def) when is_binary(def), do: {:ok, def}
-  def validate_def(_def), do: {:error, [def: :invalid]}
+  @spec validate_def(continuation_def :: continuation_def()) :: validation()
+  def validate_def(continuation_def) when is_binary(continuation_def), do: {:ok, continuation_def}
+  def validate_def(_continuation_def), do: {:error, [def: :invalid]}
 
   @spec validate_args(args :: args()) :: validation()
   def validate_args(%PactValue{} = args), do: {:ok, args}
--- test/types/pact_exec_test.exs
+++ test/types/pact_exec_test.exs
@@ -1,19 +1,301 @@
-# defmodule Kadena.Types.PactExecTest do
-#   @moduledoc """
-#   `PactExec` struct definition tests.
-#   """
+defmodule Kadena.Types.PactExecTest do
+  @moduledoc """
+  `PactExec` struct definition tests.
+  """
+  use ExUnit.Case
 
-#   alias Kadena.Types.PactExec
+  alias Kadena.Types.{Continuation, PactExec, PactTransactionHash, Step, Yield}
 
-#   use ExUnit.Case
+  describe "new/1" do
+    setup do
+      continuation_value = [def: "coin", args: "pact_value"]
 
-#   describe "new/1" do
-#     test "with a valid args list" do
-#       %PactExec{} = PactExec.new()
-#     end
+      yield_value = [
+        data: %{
+          amount: 0.01,
+          receiver: "4f9c46df2fe874d7c1b60f68f8440a444dd716e6b2efba8ee141afdd58c993dc",
+          source_chain: 0,
+          receiver_guard: [
+            pred: "keys-all",
+            keys: [
+              "4f9c46df2fe874d7c1b60f68f8440a444dd716e6b2efba8ee141afdd58c993dc"
+            ]
+          ]
+        },
+        provenance: [
+          target_chain_id: "1",
+          module_hash: "rE7DU8jlQL9x_MPYuniZJf5ICBTAEHAIFQCB4blofP4"
+        ]
+      ]
 
-#     test "with an invalid empty list" do
-#       %PactExec{} = PactExec.new()
-#     end
-#   end
-# end
+      %{
+        pact_id: "yxM0umrtdcvSUZDc_GSjwadH6ELYFCjOqI59Jzqapi4",
+        step: 1,
+        step_count: 5,
+        bool_value: false,
+        continuation: continuation_value,
+        continuation_struct: Continuation.new(continuation_value),
+        yield: yield_value,
+        yield_struct: Yield.new(yield_value)
+      }
+    end
+
+    test "with a valid args list", %{
+      pact_id: pact_id,
+      step: step,
+      step_count: step_count,
+      bool_value: bool_value,
+      continuation: continuation,
+      continuation_struct: continuation_struct,
+      yield: yield,
+      yield_struct: yield_struct
+    } do
+      %PactExec{
+        pact_id: %PactTransactionHash{hash: ^pact_id},
+        step: %Step{number: ^step},
+        step_count: ^step_count,
+        executed: ^bool_value,
+        step_has_rollback: ^bool_value,
+        continuation: ^continuation_struct,
+        yield: ^yield_struct
+      } =
+        PactExec.new(
+          pact_id: pact_id,
+          step: step,
+          step_count: step_count,
+          executed: bool_value,
+          step_has_rollback: bool_value,
+          continuation: continuation,
+          yield: yield
+        )
+    end
+
+    test "with a valid nil executed value", %{
+      pact_id: pact_id,
+      step: step,
+      step_count: step_count,
+      bool_value: bool_value,
+      continuation: continuation,
+      continuation_struct: continuation_struct,
+      yield: yield,
+      yield_struct: yield_struct
+    } do
+      %PactExec{
+        pact_id: %PactTransactionHash{hash: ^pact_id},
+        step: %Step{number: ^step},
+        step_count: ^step_count,
+        executed: nil,
+        step_has_rollback: ^bool_value,
+        continuation: ^continuation_struct,
+        yield: ^yield_struct
+      } =
+        PactExec.new(
+          pact_id: pact_id,
+          step: step,
+          step_count: step_count,
+          step_has_rollback: bool_value,
+          continuation: continuation,
+          yield: yield
+        )
+    end
+
+    test "with a valid nil yield", %{
+      pact_id: pact_id,
+      step: step,
+      step_count: step_count,
+      bool_value: bool_value,
+      continuation: continuation,
+      continuation_struct: continuation_struct
+    } do
+      %PactExec{
+        pact_id: %PactTransactionHash{hash: ^pact_id},
+        step: %Step{number: ^step},
+        step_count: ^step_count,
+        step_has_rollback: ^bool_value,
+        continuation: ^continuation_struct,
+        yield: nil
+      } =
+        PactExec.new(
+          pact_id: pact_id,
+          step: step,
+          step_count: step_count,
+          step_has_rollback: bool_value,
+          continuation: continuation
+        )
+    end
+
+    test "with a valid yield and continuation structs", %{
+      pact_id: pact_id,
+      step: step,
+      step_count: step_count,
+      bool_value: bool_value,
+      continuation_struct: continuation_struct,
+      yield_struct: yield_struct
+    } do
+      %PactExec{
+        pact_id: %PactTransactionHash{hash: ^pact_id},
+        step: %Step{number: ^step},
+        step_count: ^step_count,
+        step_has_rollback: ^bool_value,
+        continuation: ^continuation_struct,
+        yield: ^yield_struct
+      } =
+        PactExec.new(
+          pact_id: pact_id,
+          step: step,
+          step_count: step_count,
+          step_has_rollback: bool_value,
+          continuation: continuation_struct,
+          yield: yield_struct
+        )
+    end
+
+    test "with an invalid pact_id", %{
+      step: step,
+      step_count: step_count,
+      bool_value: bool_value,
+      continuation: continuation,
+      yield: yield
+    } do
+      {:error, [pact_id: :invalid]} =
+        PactExec.new(
+          pact_id: :invalid_pact_id,
+          step: step,
+          step_count: step_count,
+          step_has_rollback: bool_value,
+          continuation: continuation,
+          yield: yield
+        )
+    end
+
+    test "with an invalid step", %{
+      pact_id: pact_id,
+      step_count: step_count,
+      bool_value: bool_value,
+      continuation: continuation,
+      yield: yield
+    } do
+      {:error, [step: :invalid]} =
+        PactExec.new(
+          pact_id: pact_id,
+          step: "step1",
+          step_count: step_count,
+          step_has_rollback: bool_value,
+          continuation: continuation,
+          yield: yield
+        )
+    end
+
+    test "with an invalid step_count", %{
+      pact_id: pact_id,
+      step: step,
+      bool_value: bool_value,
+      continuation: continuation,
+      yield: yield
+    } do
+      {:error, [step_count: :invalid]} =
+        PactExec.new(
+          pact_id: pact_id,
+          step: step,
+          step_count: "step_count",
+          step_has_rollback: bool_value,
+          continuation: continuation,
+          yield: yield
+        )
+    end
+
+    test "with an invalid step_has_rollback", %{
+      pact_id: pact_id,
+      step: step,
+      step_count: step_count,
+      continuation: continuation,
+      yield: yield
+    } do
+      {:error, [step_has_rollback: :invalid]} =
+        PactExec.new(
+          pact_id: pact_id,
+          step: step,
+          step_count: step_count,
+          step_has_rollback: "none",
+          continuation: continuation,
+          yield: yield
+        )
+    end
+
+    test "with an invalid continuation", %{
+      pact_id: pact_id,
+      step: step,
+      step_count: step_count,
+      bool_value: bool_value,
+      yield: yield
+    } do
+      {:error, [continuation: :invalid]} =
+        PactExec.new(
+          pact_id: pact_id,
+          step: step,
+          step_count: step_count,
+          step_has_rollback: bool_value,
+          continuation: :invalid,
+          yield: yield
+        )
+    end
+
+    test "with an invalid continuation list", %{
+      pact_id: pact_id,
+      step: step,
+      step_count: step_count,
+      bool_value: bool_value,
+      yield: yield
+    } do
+      {:error, [continuation: :invalid, def: :invalid]} =
+        PactExec.new(
+          pact_id: pact_id,
+          step: step,
+          step_count: step_count,
+          step_has_rollback: bool_value,
+          continuation: [invalid_arg: :invalid_value],
+          yield: yield
+        )
+    end
+
+    test "with an invalid yield", %{
+      pact_id: pact_id,
+      step: step,
+      step_count: step_count,
+      bool_value: bool_value,
+      continuation: continuation
+    } do
+      {:error, [yield: :invalid]} =
+        PactExec.new(
+          pact_id: pact_id,
+          step: step,
+          step_count: step_count,
+          step_has_rollback: bool_value,
+          continuation: continuation,
+          yield: :yield
+        )
+    end
+
+    test "with an invalid yield args list", %{
+      pact_id: pact_id,
+      step: step,
+      step_count: step_count,
+      bool_value: bool_value,
+      continuation: continuation
+    } do
+      {:error, [yield: :invalid, data: :invalid]} =
+        PactExec.new(
+          pact_id: pact_id,
+          step: step,
+          step_count: step_count,
+          step_has_rollback: bool_value,
+          continuation: continuation,
+          yield: [:yield]
+        )
+    end
+
+    test "with an invalid empty list" do
+      {:error, [pact_id: :invalid]} = PactExec.new([])
+    end
+  end
+end
