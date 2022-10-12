499710a0ac6c317cf0cadeddbc8fd84b611012fa
Santiago Botero <98826652+santiagoboterokommit@users.noreply.github.com>
2022-10-12 16:57:24 -0500

    PollRequestBody type

---
+++ lib/types/poll_request_body.ex
@@ -0,0 +1,27 @@
+defmodule Kadena.Types.PollRequestBody do
+  @moduledoc """
+  `PollRequestBody` struct definition.
+  """
+
+  alias Kadena.Types.Base64UrlsList
+
+  @behaviour Kadena.Types.Spec
+
+  @type request_keys :: Base64UrlsList.t()
+
+  @type t :: %__MODULE__{request_keys: request_keys()}
+
+  defstruct request_keys: []
+
+  @impl true
+  def new(request_keys) when is_list(request_keys) do
+    case Base64UrlsList.new(request_keys) do
+      %Base64UrlsList{} = request_keys -> %__MODULE__{request_keys: request_keys}
+      {:error, _reasons} -> {:error, [request_keys: :invalid]}
+    end
+  end
+
+  def new(%Base64UrlsList{} = request_keys), do: %__MODULE__{request_keys: request_keys}
+
+  def new(_request_keys), do: {:error, [request_keys: :not_a_list]}
+end
