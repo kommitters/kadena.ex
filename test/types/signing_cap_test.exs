224366241e2e355cb7b661b54a872eb334f40bfb
Santiago Botero <98826652+santiagoboterokommit@users.noreply.github.com>
2022-10-13 11:16:06 -0500

    SigningCap type

---
+++ test/types/signing_cap_test.exs
@@ -0,0 +1,64 @@
+defmodule Kadena.Types.SigningCapTest do
+  @moduledoc """
+  `SigningCap` struct definition tests.
+  """
+
+  use ExUnit.Case
+
+  alias Kadena.Types.{Cap, SigningCap}
+
+  describe "new/1" do
+    setup do
+      %{
+        role: "",
+        description: "",
+        cap: [name: "gas", args: ["COIN.gas", 1.0e-2]],
+        cap_struct: Cap.new(name: "gas", args: ["COIN.gas", 1.0e-2])
+      }
+    end
+
+    test "with a valid param list", %{
+      role: role,
+      description: description,
+      cap: cap,
+      cap_struct: cap_struct
+    } do
+      %SigningCap{role: ^role, description: ^description, cap: ^cap_struct} =
+        SigningCap.new(role: role, description: description, cap: cap)
+    end
+
+    test "with a valid cap struct", %{
+      role: role,
+      description: description,
+      cap_struct: cap_struct
+    } do
+      %SigningCap{role: ^role, description: ^description, cap: ^cap_struct} =
+        SigningCap.new(role: role, description: description, cap: cap_struct)
+    end
+
+    test "with an invalid params list" do
+      {:error, [signing_cap: :not_a_list]} = SigningCap.new("invalid_param")
+    end
+
+    test "with an invalid role", %{
+      description: description,
+      cap_struct: cap_struct
+    } do
+      {:error, [role: :invalid]} =
+        SigningCap.new(role: 123, description: description, cap: cap_struct)
+    end
+
+    test "with an invalid description", %{role: role, cap_struct: cap_struct} do
+      {:error, [description: :invalid]} =
+        SigningCap.new(role: role, description: 123, cap: cap_struct)
+    end
+
+    test "with an invalid cap list", %{
+      role: role,
+      description: description
+    } do
+      {:error, [cap: :invalid, name: :invalid]} =
+        SigningCap.new(role: role, description: description, cap: [invalid_key: :invalid_value])
+    end
+  end
+end
