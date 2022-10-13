b76de064cdf9756ed764b762f777a209f30bd522
Santiago Botero <98826652+santiagoboterokommit@users.noreply.github.com>
2022-10-13 10:38:28 -0500

    PactAPI types part 3 (#80)
    * SPVRequestBody type
    
    * ListenResponse type
    
    * SPVResponse type
    
    * Update README.md
    
    * Fix requested changes

---
+++ test/types/spv_request_body_test.exs
@@ -0,0 +1,45 @@
+defmodule Kadena.Types.SPVRequestBodyTest do
+  @moduledoc """
+  `SPVRequestBody` struct definition tests.
+  """
+
+  use ExUnit.Case
+
+  alias Kadena.Types.{Base64Url, ChainID, SPVRequestBody}
+
+  describe "new/1" do
+    test "with a valid list" do
+      %SPVRequestBody{request_key: %Base64Url{url: "ATGCYPMNzdGcFh9Iik73KfMkgURIxaF91Ze4sHFsH8Q"}} =
+        SPVRequestBody.new(
+          request_key: "ATGCYPMNzdGcFh9Iik73KfMkgURIxaF91Ze4sHFsH8Q",
+          target_chain_id: "1"
+        )
+    end
+
+    test "with a valid Base64Url ans ChainID struct" do
+      request_key = Base64Url.new("ATGCYPMNzdGcFh9Iik73KfMkgURIxaF91Ze4sHFsH8Q")
+      target_chain_id = ChainID.new("1")
+
+      %SPVRequestBody{
+        request_key: ^request_key,
+        target_chain_id: ^target_chain_id
+      } = SPVRequestBody.new(request_key: request_key, target_chain_id: target_chain_id)
+    end
+
+    test "with an invalid request_key" do
+      {:error, [request_key: :invalid]} = SPVRequestBody.new(request_key: nil)
+    end
+
+    test "with an invalid target_chain_id" do
+      {:error, [target_chain_id: :invalid, id: :invalid]} =
+        SPVRequestBody.new(
+          request_key: "ATGCYPMNzdGcFh9Iik73KfMkgURIxaF91Ze4sHFsH8Q",
+          target_chain_id: nil
+        )
+    end
+
+    test "with an invalid list" do
+      {:error, [spv_request_body: :not_a_list]} = SPVRequestBody.new("invalid_param")
+    end
+  end
+end
