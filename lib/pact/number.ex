41d7c801a586cb61ade7d2feeddb92dd4654ee75
Santiago Botero <98826652+santiagoboterokommit@users.noreply.github.com>
2022-10-21 09:02:07 -0500

    Add @spec on functions

--- lib/pact/number.ex
+++ lib/pact/number.ex
@@ -4,6 +4,13 @@ defmodule Kadena.Pact.Number do
   """
   alias Kadena.Types.{PactDecimal, PactInt}
 
+  @type str :: String.t()
+  @type pact_decimal :: PactDecimal.t()
+  @type pact_int :: PactInt.t()
+  @type value :: str() | pact_decimal() | pact_int()
+  @type response :: {:ok, value()} | {:error, Keyword.t()}
+
+  @spec to_pact_integer(number :: str()) :: response()
   def to_pact_integer(number) do
     with {number, _rest} <- Integer.parse(number),
          %PactInt{} = pact_int <- PactInt.new(number) do
@@ -11,6 +18,7 @@ defmodule Kadena.Pact.Number do
     end
   end
 
+  @spec to_pact_decimal(decimal :: str()) :: response()
   def to_pact_decimal(decimal) do
     case PactDecimal.new(decimal) do
       %PactDecimal{} = pact_decimal -> {:ok, pact_decimal}
@@ -18,6 +26,7 @@ defmodule Kadena.Pact.Number do
     end
   end
 
+  @spec to_stringified(number :: value()) :: response()
   def to_stringified(number) when is_binary(number), do: Jason.encode(number)
 
   def to_stringified(%PactInt{value: number}), do: Jason.encode(number)
