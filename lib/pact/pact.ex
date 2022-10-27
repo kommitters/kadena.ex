81c38116b75d113c447d01dde60474fcf08d0d87
EdwinGuayacan <80716239+EdwinGuayacan@users.noreply.github.com>
2022-10-27 09:16:03 -0500

    adding pact interface

---
+++ lib/pact/pact.ex
@@ -0,0 +1,12 @@
+defmodule Kadena.Pact do
+  @moduledoc """
+    Module to access to pact functions for users
+  """
+  alias Kadena.Pact.{Number, Exp}
+  # Exp functions
+  defdelegate create_expression(args), to: Exp, as: :create
+  # Number functions
+  defdelegate to_pact_integer(str), to: Number, as: :to_pact_integer
+  defdelegate to_pact_decimal(str), to: Number, as: :to_pact_decimal
+  defdelegate to_json_string(args), to: Number, as: :to_json_string
+end
