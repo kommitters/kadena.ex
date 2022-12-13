e7487713802063b34ae7b7047862b09311504623
Miguel Nieto A <39246879+miguelnietoa@users.noreply.github.com>
2022-12-13 09:21:27 -0500

    Refactor SendResponse

---
+++ lib/chainweb/resources/send_response.ex
@@ -0,0 +1,25 @@
+defmodule Kadena.Chainweb.Resources.SendResponse do
+  @moduledoc """
+  `SendResponse` struct definition.
+  """
+
+  alias Kadena.Chainweb.Mapping
+  alias Kadena.Types.Base64UrlsList
+
+  @behaviour Kadena.Chainweb.Resource
+
+  @type request_keys :: Base64UrlsList.t()
+
+  @type t :: %__MODULE__{request_keys: request_keys()}
+
+  defstruct request_keys: []
+
+  @mapping [request_keys: {:struct, Base64UrlsList}]
+
+  @impl true
+  def new(attrs) do
+    %__MODULE__{}
+    |> Mapping.build(attrs)
+    |> Mapping.parse(@mapping)
+  end
+end
