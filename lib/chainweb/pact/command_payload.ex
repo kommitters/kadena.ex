9f20df4bb801337ff09000c103a9352a6440e36f
EdwinGuayacan <80716239+EdwinGuayacan@users.noreply.github.com>
2022-12-16 18:04:03 -0500

    Improve typespecs in CommandPayload

--- lib/chainweb/pact/command_payload.ex
+++ lib/chainweb/pact/command_payload.ex
@@ -39,21 +39,22 @@ defmodule Kadena.Chainweb.Pact.CommandPayload do
   @type nonce :: String.t()
   @type value :: network_id() | payload() | signers() | meta() | nonce()
   @type validation :: {:ok, value()} | {:error, Keyword.t()}
-
-  @type json :: String.t()
-  @type raw_signers :: list(map())
+  @type valid_map :: {:ok, map()}
+  @type valid_string :: {:ok, String.t()}
+  @type valid_list :: {:ok, list()}
+  @type map_return :: map() | nil
   @type string_value :: String.t() | nil
-  @type pact_payload :: PactPayload.t()
-  @type proof :: Proof.t() | nil
-  @type data :: EnvData.t() | nil
-  @type signer :: Signer.t()
-  @type addr :: Base16String.t() | nil
+  @type pact_values :: PactValuesList.t()
   @type scheme :: :ed25519 | nil
-  @type valid_scheme :: :ED25519 | nil
-  @type clist :: CapsList.t() | nil
-  @type valid_clist :: list(map())
+  @type scheme_return :: :ED25519 | nil
   @type cap :: Cap.t()
-  @type pact_values_list :: PactValuesList.t()
+  @type data :: EnvData.t() | nil
+  @type proof :: Proof.t() | nil
+  @type signer :: Signer.t()
+  @type raw_value :: integer() | string_value() | boolean() | Decimal.t()
+  @type clist :: CapsList.t() | nil
+  @type addr :: Base16String.t() | nil
+  @type pact_payload :: PactPayload.t()
   @type literal ::
           integer()
           | boolean()
@@ -61,11 +62,6 @@ defmodule Kadena.Chainweb.Pact.CommandPayload do
           | PactInt.t()
           | PactDecimal.t()
           | PactValuesList.t()
-  @type raw_value :: integer() | string_value() | boolean() | Decimal.t()
-  @type valid_pact_value :: raw_value() | list(raw_value())
-  @type pact_value :: list(valid_pact_value())
-  @type map_return :: map() | nil
-  @type valid_extract :: {:ok, string_value()} | {:ok, map_return()} | {:ok, raw_signers()}
 
   @type t :: %__MODULE__{
           network_id: network_id(),
@@ -122,119 +118,6 @@ defmodule Kadena.Chainweb.Pact.CommandPayload do
     end
   end
 
-  @spec extract_network_id(network_id()) :: valid_extract()
-  defp extract_network_id(%NetworkID{id: id}), do: {:ok, id}
-
-  @spec extract_payload(pact_payload()) :: valid_extract()
-  defp extract_payload(%PactPayload{payload: %ExecPayload{} = exec_payload}) do
-    %ExecPayload{code: %PactCode{code: code}, data: data} = exec_payload
-    payload = %{exec: %{code: code, data: extract_data(data)}}
-    {:ok, payload}
-  end
-
-  defp extract_payload(%PactPayload{
-         payload: %ContPayload{
-           data: data,
-           pact_id: %PactTransactionHash{hash: hash},
-           proof: proof,
-           rollback: %Rollback{value: rollback},
-           step: %Step{number: number}
-         }
-       }) do
-    payload = %{
-      cont: %{
-        data: extract_data(data),
-        pactId: hash,
-        proof: extract_proof(proof),
-        rollback: rollback,
-        step: number
-      }
-    }
-
-    {:ok, payload}
-  end
-
-  @spec extract_proof(proof()) :: string_value()
-  defp extract_proof(nil), do: nil
-  defp extract_proof(%Proof{value: proof}), do: proof
-
-  @spec extract_data(data()) :: map_return()
-  defp extract_data(nil), do: nil
-  defp extract_data(%EnvData{data: data}), do: data
-
-  @spec extract_meta(meta()) :: valid_extract()
-  defp extract_meta(%MetaData{
-         creation_time: creation_time,
-         ttl: ttl,
-         gas_limit: gas_limit,
-         gas_price: gas_price,
-         sender: sender,
-         chain_id: %ChainID{id: id}
-       }) do
-    %{
-      chain_id: id,
-      creation_time: creation_time,
-      gas_limit: gas_limit,
-      gas_price: gas_price,
-      sender: sender,
-      ttl: ttl
-    }
-    |> MapCase.to_camel!()
-    |> (&{:ok, &1}).()
-  end
-
-  @spec extract_signers_list(signers :: signers()) :: valid_extract()
-  defp extract_signers_list(%SignersList{signers: list}) do
-    signers = Enum.map(list, fn sig -> extract_signer_info(sig) end)
-    {:ok, signers}
-  end
-
-  @spec extract_signer_info(signer()) :: map_return()
-  defp extract_signer_info(%Signer{
-         addr: addr,
-         scheme: scheme,
-         pub_key: %Base16String{value: pub_key},
-         clist: %OptionalCapsList{clist: clist}
-       }) do
-    MapCase.to_camel!(%{
-      addr: extract_addr(addr),
-      scheme: extract_scheme(scheme),
-      pub_key: pub_key,
-      clist: extract_clist(clist)
-    })
-  end
-
-  @spec extract_addr(addr()) :: string_value()
-  defp extract_addr(nil), do: nil
-  defp extract_addr(%Base16String{value: value}), do: value
-
-  @spec extract_scheme(scheme()) :: valid_scheme()
-  defp extract_scheme(nil), do: nil
-  defp extract_scheme(:ed25519), do: :ED25519
-
-  @spec extract_clist(clist()) :: valid_clist()
-  defp extract_clist(nil), do: []
-
-  defp extract_clist(%CapsList{caps: caps}) do
-    Enum.map(caps, fn cap -> extract_cap_info(cap) end)
-  end
-
-  @spec extract_cap_info(cap()) :: map_return()
-  defp extract_cap_info(%Cap{name: name, args: args}) do
-    %{name: name, args: extract_values(args)}
-  end
-
-  @spec extract_values(pact_values_list()) :: pact_value()
-  defp extract_values(%PactValuesList{pact_values: pact_values}) do
-    Enum.map(pact_values, fn %PactValue{literal: pact_value} -> extract_value(pact_value) end)
-  end
-
-  @spec extract_value(literal()) :: valid_pact_value()
-  defp extract_value(%PactValuesList{} = pact_value), do: extract_values(pact_value)
-  defp extract_value(%PactInt{raw_value: value}), do: value
-  defp extract_value(%PactDecimal{raw_value: value}), do: value
-  defp extract_value(value), do: value
-
   @spec validate_network_id(network_id :: network_id()) :: validation()
   defp validate_network_id(%NetworkID{} = network_id), do: {:ok, network_id}
 
@@ -278,4 +161,117 @@ defmodule Kadena.Chainweb.Pact.CommandPayload do
 
   @spec validate_nonce(nonce :: nonce()) :: validation()
   defp validate_nonce(nonce), do: {:ok, to_string(nonce)}
+
+  @spec extract_network_id(network_id()) :: valid_string()
+  defp extract_network_id(%NetworkID{id: id}), do: {:ok, id}
+
+  @spec extract_payload(pact_payload()) :: valid_map()
+  defp extract_payload(%PactPayload{payload: %ExecPayload{} = exec_payload}) do
+    %ExecPayload{code: %PactCode{code: code}, data: data} = exec_payload
+    payload = %{exec: %{code: code, data: extract_data(data)}}
+    {:ok, payload}
+  end
+
+  defp extract_payload(%PactPayload{
+         payload: %ContPayload{
+           data: data,
+           pact_id: %PactTransactionHash{hash: hash},
+           proof: proof,
+           rollback: %Rollback{value: rollback},
+           step: %Step{number: number}
+         }
+       }) do
+    payload = %{
+      cont: %{
+        data: extract_data(data),
+        pactId: hash,
+        proof: extract_proof(proof),
+        rollback: rollback,
+        step: number
+      }
+    }
+
+    {:ok, payload}
+  end
+
+  @spec extract_proof(proof()) :: string_value()
+  defp extract_proof(nil), do: nil
+  defp extract_proof(%Proof{value: proof}), do: proof
+
+  @spec extract_data(data()) :: map_return()
+  defp extract_data(nil), do: nil
+  defp extract_data(%EnvData{data: data}), do: data
+
+  @spec extract_meta(meta()) :: valid_map()
+  defp extract_meta(%MetaData{
+         creation_time: creation_time,
+         ttl: ttl,
+         gas_limit: gas_limit,
+         gas_price: gas_price,
+         sender: sender,
+         chain_id: %ChainID{id: id}
+       }) do
+    %{
+      chain_id: id,
+      creation_time: creation_time,
+      gas_limit: gas_limit,
+      gas_price: gas_price,
+      sender: sender,
+      ttl: ttl
+    }
+    |> MapCase.to_camel!()
+    |> (&{:ok, &1}).()
+  end
+
+  @spec extract_signers_list(signers :: signers()) :: valid_list()
+  defp extract_signers_list(%SignersList{signers: list}) do
+    signers = Enum.map(list, fn sig -> extract_signer_info(sig) end)
+    {:ok, signers}
+  end
+
+  @spec extract_signer_info(signer()) :: map_return()
+  defp extract_signer_info(%Signer{
+         addr: addr,
+         scheme: scheme,
+         pub_key: %Base16String{value: pub_key},
+         clist: %OptionalCapsList{clist: clist}
+       }) do
+    MapCase.to_camel!(%{
+      addr: extract_addr(addr),
+      scheme: extract_scheme(scheme),
+      pub_key: pub_key,
+      clist: extract_clist(clist)
+    })
+  end
+
+  @spec extract_addr(addr()) :: string_value()
+  defp extract_addr(nil), do: nil
+  defp extract_addr(%Base16String{value: value}), do: value
+
+  @spec extract_scheme(scheme()) :: scheme_return()
+  defp extract_scheme(nil), do: nil
+  defp extract_scheme(:ed25519), do: :ED25519
+
+  @spec extract_clist(clist()) :: list()
+  defp extract_clist(nil), do: []
+
+  defp extract_clist(%CapsList{caps: caps}) do
+    Enum.map(caps, fn cap -> extract_cap_info(cap) end)
+  end
+
+  @spec extract_cap_info(cap()) :: map_return()
+  defp extract_cap_info(%Cap{name: name, args: args}) do
+    %{name: name, args: extract_values(args)}
+  end
+
+  @spec extract_values(pact_values()) :: list()
+  defp extract_values(%PactValuesList{pact_values: pact_values}) do
+    Enum.map(pact_values, fn %PactValue{literal: pact_value} -> extract_value(pact_value) end)
+  end
+
+  @spec extract_value(literal()) :: raw_value()
+  defp extract_value(%PactValuesList{} = pact_value), do: extract_values(pact_value)
+  defp extract_value(%PactInt{raw_value: value}), do: value
+  defp extract_value(%PactDecimal{raw_value: value}), do: value
+  defp extract_value(value), do: value
 end
