846dc5a65782a4faec43bd08e9b9e1959fe2e5f3
EdwinGuayacan <80716239+EdwinGuayacan@users.noreply.github.com>
2023-01-02 12:00:46 -0500

    Remove unused alias

--- test/types/signer_test.exs
+++ test/types/signer_test.exs
@@ -5,7 +5,7 @@ defmodule Kadena.Types.SignerTest do
 
   use ExUnit.Case
 
-  alias Kadena.Types.{Base16String, Cap, OptionalCapsList, Signer}
+  alias Kadena.Types.{Base16String, Cap, Signer}
 
   describe "new/1" do
     setup do
