444ca3b87c60851acbc83389a9a0a0a84635d090
EdwinGuayacan <80716239+EdwinGuayacan@users.noreply.github.com>
2023-01-20 11:20:25 -0500

    Add retrieve block payload batch
    Co-authored-by: David Roldán <Odraxs@users.noreply.github.com>

--- lib/chainweb/p2p/block_payload.ex
+++ lib/chainweb/p2p/block_payload.ex
@@ -3,16 +3,20 @@ defmodule Kadena.Chainweb.P2P.BlockPayload do
   BlockPayload endpoints implementation for P2P API.
   """
 
-  alias Kadena.Chainweb.P2P.BlockPayloadResponse
+  alias Kadena.Chainweb.P2P.{BlockPayloadBatchResponse, BlockPayloadResponse}
   alias Kadena.Chainweb.{Error, Request}
 
   @type network_opts :: Keyword.t()
   @type error :: {:error, Error.t()}
-  @type response :: {:ok, BlockPayloadResponse.t()} | error()
+  @type payload_hashes :: list(String.t())
+  @type retrieve_response :: {:ok, BlockPayloadResponse.t()} | error()
+  @type retrieve_batch_response :: {:ok, BlockPayloadBatchResponse.t()} | error()
+  @type json :: String.t()
 
   @endpoint "payload"
 
-  @spec retrieve(payload_hash :: String.t(), network_opts :: network_opts()) :: response()
+  @spec retrieve(payload_hash :: String.t(), network_opts :: network_opts()) ::
+          retrieve_response()
   def retrieve(payload_hash, network_opts \\ []) do
     location = Keyword.get(network_opts, :location)
     network_id = Keyword.get(network_opts, :network_id, :testnet04)
@@ -26,4 +30,27 @@ defmodule Kadena.Chainweb.P2P.BlockPayload do
     |> Request.perform()
     |> Request.results(as: BlockPayloadResponse)
   end
+
+  @spec retrieve_batch(payload_hashes :: payload_hashes(), network_opts :: network_opts()) ::
+          retrieve_batch_response()
+  def retrieve_batch(payload_hashes \\ [], network_opts \\ []) do
+    location = Keyword.get(network_opts, :location)
+    network_id = Keyword.get(network_opts, :network_id, :testnet04)
+    chain_id = Keyword.get(network_opts, :chain_id, 0)
+    headers = [{"Content-Type", "application/json"}]
+    body = json_request_body(payload_hashes)
+
+    :post
+    |> Request.new(p2p: [endpoint: @endpoint, path: "batch"])
+    |> Request.set_network(network_id)
+    |> Request.set_location(location)
+    |> Request.set_chain_id(chain_id)
+    |> Request.add_headers(headers)
+    |> Request.add_body(body)
+    |> Request.perform()
+    |> Request.results(as: BlockPayloadBatchResponse)
+  end
+
+  @spec json_request_body(payload_hashes :: payload_hashes()) :: json()
+  defp json_request_body(payload_hashes), do: Jason.encode!(payload_hashes)
 end
--- test/chainweb/p2p/block_payload_test.exs
+++ test/chainweb/p2p/block_payload_test.exs
@@ -91,6 +91,93 @@ defmodule Kadena.Chainweb.Client.CannedBlockPayloadRequests do
 
     {:error, response}
   end
+
+  def request(
+        :post,
+        "https://api.testnet.chainweb.com/chainweb/0.0/testnet04/chain/0/payload/batch",
+        _headers,
+        "[\"tD9gYGoTZX1TktM_V61deSQ7pi5N8DP-bPgeyOkf4cg\",\"EZtAeZN3UdsNsHP2v8hQ3s5uPl0u_G0juWrVIu1XqQ4\"]",
+        _options
+      ) do
+    response = Chainweb.fixture("block_payload_hash_retrieve_batch")
+    {:ok, response}
+  end
+
+  def request(
+        :post,
+        "https://api.testnet.chainweb.com/chainweb/0.0/testnet04/chain/0/payload/batch",
+        _headers,
+        "[\"ññññM4doD-jMHyxi4TvfBDUy3x9VMkcLxgnpjtvbbd77774\"]",
+        _options
+      ) do
+    response =
+      Error.new(
+        {:chainweb,
+         %{
+           status: 400,
+           title: "Error in $[0]: Base64DecodeException \"invalid base64 encoding near offset 0\""
+         }}
+      )
+
+    {:error, response}
+  end
+
+  def request(
+        :post,
+        "https://api.testnet.chainweb.com/chainweb/0.0/testnet04/chain/0/payload/batch",
+        _headers,
+        "\"invalid\"",
+        _options
+      ) do
+    response =
+      Error.new(
+        {:chainweb,
+         %{
+           status: 400,
+           title: "Error in $: parsing [] failed, expected Array, but encountered String"
+         }}
+      )
+
+    {:error, response}
+  end
+
+  def request(
+        :post,
+        "https://col1.chainweb.com/chainweb/0.0/mainnet01/chain/0/payload/batch",
+        _headers,
+        _body,
+        _options
+      ) do
+    response =
+      Error.new(
+        {:chainweb,
+         %{
+           status: :network_error,
+           title: :nxdomain
+         }}
+      )
+
+    {:error, response}
+  end
+
+  def request(
+        :post,
+        "https://api.testnet.chainweb.com/chainweb/0.0/testnet04/chain/20/payload/batch",
+        _headers,
+        _body,
+        _options
+      ) do
+    response =
+      Error.new(
+        {:chainweb,
+         %{
+           status: 404,
+           title: "not found"
+         }}
+      )
+
+    {:error, response}
+  end
 end
 
 defmodule Kadena.Chainweb.P2P.BlockPayloadTest do
@@ -161,6 +248,82 @@ defmodule Kadena.Chainweb.P2P.BlockPayloadTest do
     test "error with a non existing location", %{payload_hash: payload_hash} do
       {:error, %Error{status: :network_error, title: :nxdomain}} =
         BlockPayload.retrieve(payload_hash, location: "col1", network_id: :mainnet01)
+    end
+  end
+
+  describe "retrieve_batch/2" do
+    setup do
+      Application.put_env(:kadena, :http_client_impl, CannedBlockPayloadRequests)
+
+      on_exit(fn ->
+        Application.delete_env(:kadena, :http_client_impl)
+      end)
+
+      payload_hashes = [
+        "tD9gYGoTZX1TktM_V61deSQ7pi5N8DP-bPgeyOkf4cg",
+        "EZtAeZN3UdsNsHP2v8hQ3s5uPl0u_G0juWrVIu1XqQ4"
+      ]
+
+      success_response =
+        {:ok,
+         %Kadena.Chainweb.P2P.BlockPayloadBatchResponse{
+           batch: [
+             %{
+               miner_data:
+                 "eyJhY2NvdW50IjoidXMxIiwicHJlZGljYXRlIjoia2V5cy1hbGwiLCJwdWJsaWMta2V5cyI6WyJkYjc3Njc5M2JlMGZjZjhlNzZjNzViZGIzNWEzNmU2N2YyOTgxMTFkYzYxNDVjNjY2OTNiMDEzMzE5MmUyNjE2Il19",
+               outputs_hash: "mAf5H9c4D8j3s6FhIlOdZgrvoffwnhrBM20zzhOK5SA",
+               payload_hash: "tD9gYGoTZX1TktM_V61deSQ7pi5N8DP-bPgeyOkf4cg",
+               transactions: [],
+               transactions_hash: "AvpbbrgkfNtMI6Hq0hJWZatbwggEKppNYL5rAXJakrw"
+             },
+             %{
+               miner_data:
+                 "eyJhY2NvdW50IjoidXMxIiwicHJlZGljYXRlIjoia2V5cy1hbGwiLCJwdWJsaWMta2V5cyI6WyJkYjc3Njc5M2JlMGZjZjhlNzZjNzViZGIzNWEzNmU2N2YyOTgxMTFkYzYxNDVjNjY2OTNiMDEzMzE5MmUyNjE2Il19",
+               outputs_hash: "KG91xchUDjg0z9HPbe8u1_8q-aotv1e2Q1QtMIqII2c",
+               payload_hash: "EZtAeZN3UdsNsHP2v8hQ3s5uPl0u_G0juWrVIu1XqQ4",
+               transactions: [],
+               transactions_hash: "AvpbbrgkfNtMI6Hq0hJWZatbwggEKppNYL5rAXJakrw"
+             }
+           ]
+         }}
+
+      %{
+        payload_hashes: payload_hashes,
+        success_response: success_response
+      }
+    end
+
+    test "success", %{
+      success_response: success_response,
+      payload_hashes: payload_hashes
+    } do
+      ^success_response = BlockPayload.retrieve_batch(payload_hashes)
+    end
+
+    test "decode error with an invalid payload_hashes" do
+      {:error,
+       %Kadena.Chainweb.Error{
+         status: 400,
+         title: "Error in $[0]: Base64DecodeException \"invalid base64 encoding near offset 0\""
+       }} = BlockPayload.retrieve_batch(["ññññM4doD-jMHyxi4TvfBDUy3x9VMkcLxgnpjtvbbd77774"])
+    end
+
+    test "parsing error with an invalid payload_hashes" do
+      {:error,
+       %Kadena.Chainweb.Error{
+         status: 400,
+         title: "Error in $: parsing [] failed, expected Array, but encountered String"
+       }} = BlockPayload.retrieve_batch("invalid")
+    end
+
+    test "error with an invalid chain_id", %{payload_hashes: payload_hashes} do
+      {:error, %Error{status: 404, title: "not found"}} =
+        BlockPayload.retrieve_batch(payload_hashes, chain_id: "20")
+    end
+
+    test "error with a non existing location", %{payload_hashes: payload_hashes} do
+      {:error, %Error{status: :network_error, title: :nxdomain}} =
+        BlockPayload.retrieve_batch(payload_hashes, location: "col1", network_id: :mainnet01)
     end
   end
 end
