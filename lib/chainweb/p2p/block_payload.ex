5b8ba979bc180b125bc29f30b7ed9ea1857cbfbd
EdwinGuayacan <80716239+EdwinGuayacan@users.noreply.github.com>
2023-01-20 19:04:40 -0500

    Add block payload batch with outputs

--- lib/chainweb/p2p/block_payload.ex
+++ lib/chainweb/p2p/block_payload.ex
@@ -5,6 +5,7 @@ defmodule Kadena.Chainweb.P2P.BlockPayload do
 
   alias Kadena.Chainweb.P2P.{
     BlockPayloadBatchResponse,
+    BlockPayloadBatchWithOutputsResponse,
     BlockPayloadResponse,
     BlockPayloadWithOutputsResponse
   }
@@ -17,6 +18,7 @@ defmodule Kadena.Chainweb.P2P.BlockPayload do
   @type retrieve_response :: {:ok, BlockPayloadResponse.t()} | error()
   @type retrieve_batch_response :: {:ok, BlockPayloadBatchResponse.t()} | error()
   @type with_outputs_response :: {:ok, BlockPayloadWithOutputsResponse.t()} | error()
+  @type batch_with_outputs_response :: {:ok, BlockPayloadBatchWithOutputsResponse.t()} | error()
   @type json :: String.t()
 
   @endpoint "payload"
@@ -71,6 +73,26 @@ defmodule Kadena.Chainweb.P2P.BlockPayload do
     |> Request.set_chain_id(chain_id)
     |> Request.perform()
     |> Request.results(as: BlockPayloadWithOutputsResponse)
+  end
+
+  @spec batch_with_outputs(payload_hashes :: payload_hashes(), network_opts :: network_opts()) ::
+          batch_with_outputs_response()
+  def batch_with_outputs(payload_hashes \\ [], network_opts \\ []) do
+    location = Keyword.get(network_opts, :location)
+    network_id = Keyword.get(network_opts, :network_id, :testnet04)
+    chain_id = Keyword.get(network_opts, :chain_id, 0)
+    headers = [{"Content-Type", "application/json"}]
+    body = json_request_body(payload_hashes)
+
+    :post
+    |> Request.new(p2p: [endpoint: @endpoint, path: "outputs", segment: "batch"])
+    |> Request.set_network(network_id)
+    |> Request.set_location(location)
+    |> Request.set_chain_id(chain_id)
+    |> Request.add_headers(headers)
+    |> Request.add_body(body)
+    |> Request.perform()
+    |> Request.results(as: BlockPayloadBatchWithOutputsResponse)
   end
 
   @spec json_request_body(payload_hashes :: payload_hashes()) :: json()
--- test/chainweb/p2p/block_payload_test.exs
+++ test/chainweb/p2p/block_payload_test.exs
@@ -266,6 +266,93 @@ defmodule Kadena.Chainweb.Client.CannedBlockPayloadRequests do
 
     {:error, response}
   end
+
+  def request(
+        :post,
+        "https://api.testnet.chainweb.com/chainweb/0.0/testnet04/chain/0/payload/outputs/batch",
+        _headers,
+        "[\"tD9gYGoTZX1TktM_V61deSQ7pi5N8DP-bPgeyOkf4cg\",\"EZtAeZN3UdsNsHP2v8hQ3s5uPl0u_G0juWrVIu1XqQ4\"]",
+        _options
+      ) do
+    response = Chainweb.fixture("block_payload_batch_with_outputs")
+    {:ok, response}
+  end
+
+  def request(
+        :post,
+        "https://api.testnet.chainweb.com/chainweb/0.0/testnet04/chain/0/payload/outputs/batch",
+        _headers,
+        "[\"ññññM4doD-jMHyxi4TvfBDUy3x9VMkcLxgnpjtvbbd77774\"]",
+        _options
+      ) do
+    response =
+      Error.new(
+        {:chainweb,
+         %{
+           status: 400,
+           title: "Error in $[0]: Base64DecodeException \"invalid base64 encoding near offset 0\""
+         }}
+      )
+
+    {:error, response}
+  end
+
+  def request(
+        :post,
+        "https://api.testnet.chainweb.com/chainweb/0.0/testnet04/chain/0/payload/outputs/batch",
+        _headers,
+        "\"invalid\"",
+        _options
+      ) do
+    response =
+      Error.new(
+        {:chainweb,
+         %{
+           status: 400,
+           title: "Error in $: parsing [] failed, expected Array, but encountered String"
+         }}
+      )
+
+    {:error, response}
+  end
+
+  def request(
+        :post,
+        "https://col1.chainweb.com/chainweb/0.0/mainnet01/chain/0/payload/outputs/batch",
+        _headers,
+        _body,
+        _options
+      ) do
+    response =
+      Error.new(
+        {:chainweb,
+         %{
+           status: :network_error,
+           title: :nxdomain
+         }}
+      )
+
+    {:error, response}
+  end
+
+  def request(
+        :post,
+        "https://api.testnet.chainweb.com/chainweb/0.0/testnet04/chain/20/payload/outputs/batch",
+        _headers,
+        _body,
+        _options
+      ) do
+    response =
+      Error.new(
+        {:chainweb,
+         %{
+           status: 404,
+           title: "not found"
+         }}
+      )
+
+    {:error, response}
+  end
 end
 
 defmodule Kadena.Chainweb.P2P.BlockPayloadTest do
@@ -277,7 +364,14 @@ defmodule Kadena.Chainweb.P2P.BlockPayloadTest do
 
   alias Kadena.Chainweb.Client.CannedBlockPayloadRequests
   alias Kadena.Chainweb.Error
-  alias Kadena.Chainweb.P2P.{BlockPayload, BlockPayloadBatchResponse, BlockPayloadResponse}
+
+  alias Kadena.Chainweb.P2P.{
+    BlockPayload,
+    BlockPayloadBatchResponse,
+    BlockPayloadBatchWithOutputsResponse,
+    BlockPayloadResponse,
+    BlockPayloadWithOutputsResponse
+  }
 
   describe "retrieve/1" do
     setup do
@@ -425,7 +519,7 @@ defmodule Kadena.Chainweb.P2P.BlockPayloadTest do
 
       success_response =
         {:ok,
-         %Kadena.Chainweb.P2P.BlockPayloadWithOutputsResponse{
+         %BlockPayloadWithOutputsResponse{
            coinbase:
              "eyJnYXMiOjAsInJlc3VsdCI6eyJzdGF0dXMiOiJzdWNjZXNzIiwiZGF0YSI6IldyaXRlIHN1Y2NlZWRlZCJ9LCJyZXFLZXkiOiJJak5sU0RFeGRrbGZkMXAxVUROc1JVdGphV3htUTNnNE9WOXJXamM0YmtaMVNrcGlkSGswTkdsT1FtOGkiLCJsb2dzIjoiUkFuWnh1S2NfaFNrZU5OVHBZQUZFVDZTS1BDWVVhczRvOUlEVl92MlNPayIsIm1ldGFEYXRhIjpudWxsLCJjb250aW51YXRpb24iOm51bGwsInR4SWQiOjEwfQ",
            miner_data:
@@ -454,7 +548,7 @@ defmodule Kadena.Chainweb.P2P.BlockPayloadTest do
       non_exisitent_payload_hash: non_exisitent_payload_hash
     } do
       {:error,
-       %Kadena.Chainweb.Error{
+       %Error{
          status: 404,
          title:
            "{\"reason\":\"key not found\",\"key\":\"M4doD-jMHyxi4TvfBDUy3x9VMkcLxgnpjtvbbd77774\"}"
@@ -462,7 +556,7 @@ defmodule Kadena.Chainweb.P2P.BlockPayloadTest do
     end
 
     test "error with an invalid payload_hash" do
-      {:error, %Kadena.Chainweb.Error{status: 400, title: "DecodeException \"not enough bytes\""}} =
+      {:error, %Error{status: 400, title: "DecodeException \"not enough bytes\""}} =
         BlockPayload.with_outputs("invalid")
     end
 
@@ -474,6 +568,86 @@ defmodule Kadena.Chainweb.P2P.BlockPayloadTest do
     test "error with a non existing location", %{payload_hash: payload_hash} do
       {:error, %Error{status: :network_error, title: :nxdomain}} =
         BlockPayload.with_outputs(payload_hash, location: "col1", network_id: :mainnet01)
+    end
+  end
+
+  describe "batch_with_outputs/2" do
+    setup do
+      Application.put_env(:kadena, :http_client_impl, CannedBlockPayloadRequests)
+
+      on_exit(fn ->
+        Application.delete_env(:kadena, :http_client_impl)
+      end)
+
+      payload_hashes = [
+        "tD9gYGoTZX1TktM_V61deSQ7pi5N8DP-bPgeyOkf4cg",
+        "EZtAeZN3UdsNsHP2v8hQ3s5uPl0u_G0juWrVIu1XqQ4"
+      ]
+
+      success_response =
+        {:ok,
+         %BlockPayloadBatchWithOutputsResponse{
+           batch: [
+             %{
+               coinbase:
+                 "eyJnYXMiOjAsInJlc3VsdCI6eyJzdGF0dXMiOiJzdWNjZXNzIiwiZGF0YSI6IldyaXRlIHN1Y2NlZWRlZCJ9LCJyZXFLZXkiOiJJazAwWkc5RUxXcE5TSGw0YVRSVWRtWkNSRlY1TTNnNVZrMXJZMHg0WjI1d2FuUjJZbUprTUhsVlVVRWkiLCJsb2dzIjoiYk9JYUNPWk9fYU5ZTmhEMTBuZGFYTnpod3FBTUotdEhWcWp0eVNmZjBWWSIsIm1ldGFEYXRhIjpudWxsLCJjb250aW51YXRpb24iOm51bGwsInR4SWQiOjExfQ",
+               miner_data:
+                 "eyJhY2NvdW50IjoidXMxIiwicHJlZGljYXRlIjoia2V5cy1hbGwiLCJwdWJsaWMta2V5cyI6WyJkYjc3Njc5M2JlMGZjZjhlNzZjNzViZGIzNWEzNmU2N2YyOTgxMTFkYzYxNDVjNjY2OTNiMDEzMzE5MmUyNjE2Il19",
+               outputs_hash: "mAf5H9c4D8j3s6FhIlOdZgrvoffwnhrBM20zzhOK5SA",
+               payload_hash: "tD9gYGoTZX1TktM_V61deSQ7pi5N8DP-bPgeyOkf4cg",
+               transactions: [],
+               transactions_hash: "AvpbbrgkfNtMI6Hq0hJWZatbwggEKppNYL5rAXJakrw"
+             },
+             %{
+               coinbase:
+                 "eyJnYXMiOjAsInJlc3VsdCI6eyJzdGF0dXMiOiJzdWNjZXNzIiwiZGF0YSI6IldyaXRlIHN1Y2NlZWRlZCJ9LCJyZXFLZXkiOiJJalJyWVVrMVYyc3RkRE50ZGs1YWIwSnRWa1ZEWW10ZmVHZGxOVk4xYW5KV2FERnpPRk10UjBWVFMwa2kiLCJsb2dzIjoidHBJUG8zR3g0QXV6QjFZRk9jWEhyTVU3R1lmaW9BcEp6YWNVSjVqc0RvWSIsIm1ldGFEYXRhIjpudWxsLCJjb250aW51YXRpb24iOm51bGwsInR4SWQiOjEyfQ",
+               miner_data:
+                 "eyJhY2NvdW50IjoidXMxIiwicHJlZGljYXRlIjoia2V5cy1hbGwiLCJwdWJsaWMta2V5cyI6WyJkYjc3Njc5M2JlMGZjZjhlNzZjNzViZGIzNWEzNmU2N2YyOTgxMTFkYzYxNDVjNjY2OTNiMDEzMzE5MmUyNjE2Il19",
+               outputs_hash: "KG91xchUDjg0z9HPbe8u1_8q-aotv1e2Q1QtMIqII2c",
+               payload_hash: "EZtAeZN3UdsNsHP2v8hQ3s5uPl0u_G0juWrVIu1XqQ4",
+               transactions: [],
+               transactions_hash: "AvpbbrgkfNtMI6Hq0hJWZatbwggEKppNYL5rAXJakrw"
+             }
+           ]
+         }}
+
+      %{
+        payload_hashes: payload_hashes,
+        success_response: success_response
+      }
+    end
+
+    test "success", %{
+      success_response: success_response,
+      payload_hashes: payload_hashes
+    } do
+      ^success_response = BlockPayload.batch_with_outputs(payload_hashes)
+    end
+
+    test "decode error with an invalid payload_hashes" do
+      {:error,
+       %Error{
+         status: 400,
+         title: "Error in $[0]: Base64DecodeException \"invalid base64 encoding near offset 0\""
+       }} = BlockPayload.batch_with_outputs(["ññññM4doD-jMHyxi4TvfBDUy3x9VMkcLxgnpjtvbbd77774"])
+    end
+
+    test "parsing error with an invalid payload_hashes" do
+      {:error,
+       %Error{
+         status: 400,
+         title: "Error in $: parsing [] failed, expected Array, but encountered String"
+       }} = BlockPayload.batch_with_outputs("invalid")
+    end
+
+    test "error with an invalid chain_id", %{payload_hashes: payload_hashes} do
+      {:error, %Error{status: 404, title: "not found"}} =
+        BlockPayload.batch_with_outputs(payload_hashes, chain_id: "20")
+    end
+
+    test "error with a non existing location", %{payload_hashes: payload_hashes} do
+      {:error, %Error{status: :network_error, title: :nxdomain}} =
+        BlockPayload.batch_with_outputs(payload_hashes, location: "col1", network_id: :mainnet01)
     end
   end
 end
