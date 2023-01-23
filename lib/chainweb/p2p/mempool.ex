c97df2a28f6068920ae6e4e4f834a133f94b5dcb
EdwinGuayacan <80716239+EdwinGuayacan@users.noreply.github.com>
2023-01-23 15:47:03 -0500

    Add mempool (lookup pending txs)
    Co-authored-by: David Roldán <Odraxs@users.noreply.github.com>

--- lib/chainweb/p2p/mempool.ex
+++ lib/chainweb/p2p/mempool.ex
@@ -3,7 +3,12 @@ defmodule Kadena.Chainweb.P2P.Mempool do
   Mempool endpoints implementation for P2P API.
   """
 
-  alias Kadena.Chainweb.P2P.{MempoolCheckResponse, MempoolRetrieveResponse}
+  alias Kadena.Chainweb.P2P.{
+    MempoolCheckResponse,
+    MempoolLookupResponse,
+    MempoolRetrieveResponse
+  }
+
   alias Kadena.Chainweb.{Error, Request}
 
   @type network_opts :: Keyword.t()
@@ -12,7 +17,8 @@ defmodule Kadena.Chainweb.P2P.Mempool do
   @type network_id :: :mainnet01 | :testnet04
   @type request_keys :: list(String.t())
   @type location :: String.t()
-  @type mempool_response :: MempoolRetrieveResponse.t() | MempoolCheckResponse.t()
+  @type mempool_response ::
+          MempoolRetrieveResponse.t() | MempoolCheckResponse.t() | MempoolLookupResponse
   @type response :: {:ok, mempool_response()} | error()
 
   @endpoint "mempool"
@@ -52,6 +58,26 @@ defmodule Kadena.Chainweb.P2P.Mempool do
     |> Request.add_headers(headers)
     |> Request.perform()
     |> Request.results(as: MempoolCheckResponse)
+  end
+
+  @spec lookup_pending_txs(request_keys :: request_keys(), network_opts :: network_opts()) ::
+          response()
+  def lookup_pending_txs(request_keys \\ [], network_opts \\ []) do
+    network_id = Keyword.get(network_opts, :network_id, :testnet04)
+    chain_id = Keyword.get(network_opts, :chain_id, 0)
+    location = Keyword.get(network_opts, :location, set_default_location(network_id))
+    body = json_request_body(request_keys)
+    headers = [{"Content-Type", "application/json"}]
+
+    :post
+    |> Request.new(p2p: [endpoint: @endpoint, path: "lookup"])
+    |> Request.set_network(network_id)
+    |> Request.set_location(location)
+    |> Request.set_chain_id(chain_id)
+    |> Request.add_body(body)
+    |> Request.add_headers(headers)
+    |> Request.perform()
+    |> Request.results(as: MempoolLookupResponse)
   end
 
   @spec set_default_location(network_id :: network_id()) :: location()
